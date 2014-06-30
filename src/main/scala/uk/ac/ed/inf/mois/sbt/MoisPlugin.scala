5b33b002c19c446a487f41a69c880566d28b8afa
William Waites <wwaites@tardis.ed.ac.uk>
2014-06-30 10:52:33 +0000

    plugin is enabled

--- src/main/scala/uk/ac/ed/inf/mois/sbt/MoisPlugin.scala
+++ src/main/scala/uk/ac/ed/inf/mois/sbt/MoisPlugin.scala
@@ -6,6 +6,10 @@ import Keys._
 object MoisPlugin extends AutoPlugin {
   override lazy val projectSettings = Seq(commands += mois)
 
+  // n.b we should do something better here, like check if
+  // mois is in libraryDependencies
+  override def trigger = allRequirements
+
   lazy val mois =
     Command.command("mois") { (state: State) =>
       println("hello world")
